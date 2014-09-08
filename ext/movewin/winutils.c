3d1f5ac1e6be79c87e455203789b1918e4cdba1e
Andrew Ho <ho@groupon.com>
2014-09-07 20:09:18 -0700

    Sync with commit 9767e474 of git://github.com/andrewgho/movewin.git: Fix memory leak by releasing references created internally; Fix segfault from overaggressive CFRelease()

--- ext/movewin/winutils.c
+++ ext/movewin/winutils.c
@@ -94,6 +94,7 @@ int EnumerateWindows(
         if(windowName) free(windowName);
         if(appName) free(appName);
     }
+    CFRelease(windowList);
     if(subPattern != pattern) free(subPattern);
 
     return count;
@@ -219,6 +220,7 @@ AXUIElementRef AXWindowFromCGWindow(CFDictionaryRef window, int minIdx) {
             matchIdx++;
         }
     }
+    CFRelease(app);
 
     return foundAppWindow;
 }
