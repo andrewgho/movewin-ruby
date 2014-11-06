e060ed47e1aa50555f44a02640ce1ad264b73a5a
Andrew Ho <ho@groupon.com>
2014-11-06 10:28:32 -0800

    Only patch dispatch/object.h when using gcc; output patch progress and result; dynamically crawl include paths to find header file to patch (issue #1)

--- ext/movewin/extconf.rb
+++ ext/movewin/extconf.rb
@@ -22,48 +22,115 @@ def yosemite_or_newer?
   Gem::Version.new(`sw_vers -productVersion`) >= Gem::Version.new('10.10')
 end
 
-# Return true if our compiler is GCC (not clang), as for RVM installed Ruby
+# Return true if our compiler is GCC (not Clang), as for RVM installed Ruby
 def using_gcc?
-  true  # TODO: make this really work
+  # Match gcc, /usr/local/bin/gcc-4.2, etc. (Clang is "xcrun cc")
+  File.basename(RbConfig::MAKEFILE_CONFIG["CC"]).match(/\Agcc\b/)
 end
 
 # Building with GCC on Yosemite (OS X 10.10) results in an error
 # (https://github.com/andrewgho/movewin-ruby/issues/1).
 # Patch header file to work around this issue.
 def fix_dispatch_object_header!
-  srcfile = find_header_file('dispatch/object.h')
-  tmpfile = "#{File.dirname(__FILE__)}/dispatch/object.h.tmp.#{$$}"
-  begin
-    patched = false
-    File.open(tmpfile, 'w') do |tmpfh|
-      File.open(srcfile, 'r').each do |srcline|
-        patched ||= srcline.sub!(
-          /\Atypedef void \(\^dispatch_block_t\)\(void\);/,
-          'typedef void* dispatch_block_t;'
-        )
-        tmpfh.print(srcline)
+  $stdout.print 'Creating patched copy of dispatch/object.h... '
+  $stdout.flush
+  status = 'failed'
+  if (srcfile = find_header_file('dispatch/object.h'))
+    tmpfile = "#{File.dirname(__FILE__)}/dispatch/object.h.tmp.#{$$}"
+    begin
+      patched = false
+      File.open(tmpfile, 'w') do |tmpfh|
+        File.open(srcfile, 'r').each do |srcline|
+          patched ||= srcline.sub!(
+            /\Atypedef void \(\^dispatch_block_t\)\(void\);/,
+            'typedef void* dispatch_block_t;'
+          )
+          tmpfh.print(srcline)
+        end
       end
-    end
-    if patched
-      destfile = "#{File.dirname(__FILE__)}/dispatch/object.h"
-      File.rename(tmpfile, destfile)
-      if $CFLAGS.nil? || $CFLAGS.empty?
-        $CFLAGS = "-I#{File.dirname(__FILE__)}"
+      if patched
+        destfile = "#{File.dirname(__FILE__)}/dispatch/object.h"
+        File.rename(tmpfile, destfile)
+        if $CFLAGS.nil? || $CFLAGS.empty?
+          $CFLAGS = "-I#{File.dirname(__FILE__)}"
+        else
+          $CFLAGS += " -I#{File.dirname(__FILE__)}"
+        end
+        set_constant! :CLEANINGS,
+          "DISTCLEANFILES += dispatch/object.h\n" + CLEANINGS
+        status = "patched: #{destfile}"
       else
-        $CFLAGS += " -I#{File.dirname(__FILE__)}"
+        status = 'skipped'
       end
-      set_constant! :CLEANINGS,
-        "DISTCLEANFILES += dispatch/object.h\n" + CLEANINGS
+    ensure
+      File.unlink(tmpfile) if File.exists?(tmpfile)
     end
-  ensure
-    File.unlink(tmpfile) if File.exists?(tmpfile)
+  else
+    status = 'header not found'
   end
+  $stdout.puts status
+  $stdout.flush
 end
 
-# Given an #include <foo/bar.h>, return actual filename /path/to/foo/bar.h
+# Given an #include <dispatch/object.h>, return actual filename
+# /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/usr/include/dispatch/object.h
 def find_header_file(header)
-  # TODO: really crawl through cpp include path to find this
-  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/usr/include/#{header}"
+  filename = nil
+  header_include_paths.each do |path|
+    maybe_filename = "#{path}/#{header}"
+    if File.exists?(maybe_filename)
+      filename = maybe_filename
+      break
+    end
+  end
+  filename
+end
+
+# Return GCC header include path
+# (http://stackoverflow.com/a/19839946, http://stackoverflow.com/a/19852298)
+# gcc -Wp,-v -xc /dev/null -fsyntax-only 2>&1
+def header_include_paths
+  cmd = RbConfig::MAKEFILE_CONFIG["CC"]
+  args = %w{-Wp -v -xc /dev/null -fsyntax-only}
+  paths = []
+  reading_paths = false
+  run_command(cmd, *args) do |line|
+    if reading_paths
+      if line.chomp.match(/\A\QEnd of search list.\E\Z/)
+        reading_paths = false
+      elsif line.match(/\A \//)
+        line.strip!
+        line.sub!(/\s+\(framework directory\)\Z/, '')
+        paths << line
+      end
+    elsif line.chomp.match(/\A\Q#include <...> search starts here:\E\Z/)
+      reading_paths = true
+    end
+  end
+  paths
+end
+
+# Safely run a command with no shell escapes, pass output lines to callback
+def run_command(cmd, *args)
+  raise ArgumentError.new('missing required cmd to run') if cmd.nil?
+  rd, wr = IO.pipe
+  if fork
+    wr.close
+    if block_given?
+      rd.each { |line| yield(line) }
+    else
+      rd.read
+    end
+    rd.close
+    Process.wait
+  else
+    rd.close
+    $stdout.reopen(wr)
+    $stderr.reopen(wr)
+    exec cmd, *args
+    raise "exec #{cmd} failed"
+  end
+  $? == 0 # return a bool indicating a successful exit
 end
 
 # Redefine constant without warning (http://stackoverflow.com/q/3375360)
